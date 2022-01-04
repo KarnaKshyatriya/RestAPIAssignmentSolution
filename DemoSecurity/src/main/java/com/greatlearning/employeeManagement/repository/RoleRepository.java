a7ad2a43fda84398b9872fa5941f895b23bf45d7
Soumalya Khan <toast.maketext@gmail.com>
2022-01-04 20:39:49 +0530

    deleting old files

--- DemoSecurity/src/main/java/com/greatlearning/employeeManagement/repository/RoleRepository.java
+++
@@ -1,9 +0,0 @@
-package com.greatlearning.employeeManagement.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.greatlearning.employeeManagement.entity.Role;
-
-public interface RoleRepository extends JpaRepository<Role, Integer> {
-
-}
